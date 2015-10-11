c31aefe6a7f8238e8444c6b0125942cafed77d02
Fred Agyeman <fredyliby@verizon.net>
2015-10-10 22:14:12 -0400

    initial commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,28 @@
 class PostsController < ApplicationController
-		def index
-			@posts = Post.all.order('created_at DESC')
-		end
-
-		def new
-			@post = post.new
-		end
-
+	def index
+		@posts = Post.all.order('created_at DESC')
+	end
+	def new
+		@post = post.new
+	end
 		def create
 			@post = Post.new(post_params)
-			@post.save
+		   if @post.save
 			redirect_to @post
+		else render 'new'
+		   end
 		end
-
+		def edit
+			@post = Post.find(params[:id])
+		end
+	def update
+			@post = Post.find(params[:id])
+			if @post.update(params[:post].permit(:title, :body))
+				redirect_to @post
+			else
+				render 'edit'
+			end		
+	end	
 private
 		def post_params
 			params.require(:post).permit(:title, :body)
@@ -20,5 +30,4 @@ private
 		def show
 			@post =post.find(params[:id])
 		end
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,3 +2,4 @@ class Post < ActiveRecord::Base
 	validates :title, presence:true, length:{ minimum:7}
 	validates :body, presence:true,
 end
+end
