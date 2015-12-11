aa49a159eb45c4291a421248d171b8c1c2ab25db
Fred Agyeman <fredyliby@verizon.net>
2015-12-10 22:06:07 -0500

    modified edit page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,24 +2,23 @@
 class CommentsController < ApplicationController
 
 	def index
-	
-
 	end
+
 	def new
 		 @post = Post.new(params[:post_id])
-		# @comment = @post.comments
+		@comment = @post.comments
 		@comment = Comment.new
 	end
 
 	def create
 		@post = Post.find(params[:post_id])
-    	@comment = @post.comments.create(params[:comment]
-		# if @comment.save
-			# flash[:success] = "Comment created!"
-			# redirect_to @post
-		# else
-		# redirect_to post_path(@post)
-		# end
+    	@comment = @post.comments.create(params[comment_params]
+		if @comment.save
+			flash[:success] = "Comment created!"
+			redirect_to @post
+		else
+		 redirect_to post_path(@post)
+		end
 	end
 
 
@@ -36,6 +35,10 @@ class CommentsController < ApplicationController
 		@comment = @post.comments.find(params[:id])
 	end
 
+	private
 
+	def comment_params
+			params.require(:comment).permit(:body)
 
+	end
 end
